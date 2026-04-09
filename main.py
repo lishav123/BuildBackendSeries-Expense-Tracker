7833a49beccc908824bd93b22c10b25dff2f57eb
lishav123 <aishwaraytiwary@gmail.com>
2026-04-09 23:41:09 +0530

    updated for category feat

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ class ExpenseTracker:
             with open("data.json", "w") as f:
                 json.dump(self.data, f, indent=4)
 
-    def update_expense(self, expense_id: int, description: str | None = None, expense: int | None = None):
+    def update_expense(self, expense_id: int, description: str | None = None, expense: int | None = None, category: str | None = None):
         if not self.data:
             print("No data found to update")
 
@@ -62,7 +62,8 @@ class ExpenseTracker:
                             "id": x["id"],
                             "description": description if description is not None else x["description"],
                             "expense": expense if expense is not None else x["expense"],
-                            "date": x["date"]
+                            "date": x["date"],
+                            "category": category if category is not None else x["category"]
                         }
                     , self.data
                 )
@@ -73,12 +74,12 @@ class ExpenseTracker:
 
             print("Expense updated successfully")
 
-    def add_expense(self, description: str, expense: float):
+    def add_expense(self, description: str, expense: float, category: str = "General"):
         if self.data == []:
-            self.data.append({"id": 0, "description": description, "expense": expense, "date": str(datetime.date(datetime.now()).isoformat())})
+            self.data.append({"id": 0, "description": description, "expense": expense, "date": str(datetime.date(datetime.now()).isoformat()), "category": category.lower()})
 
         else:
-            self.data.append({"id": self.data[-1]["id"] + 1, "description": description, "expense": expense, "date": str(datetime.date(datetime.now()).isoformat())})
+            self.data.append({"id": self.data[-1]["id"] + 1, "description": description, "expense": expense, "date": str(datetime.date(datetime.now()).isoformat()), "category": category.lower()})
 
         print("Expense added successfully")
         with open("data.json", "w") as f:
@@ -99,40 +100,53 @@ class ExpenseTracker:
             with open("data.json", "w") as f:
                 json.dump(self.data, f, indent=4)
 
-    def summary_expense(self, month: int | None = None):
+    def summary_expense(self, month: int | None = None, category: str | None = None):
 
         if month is None:
-            total_expense = sum(list(map(lambda x: x["expense"], self.data)))
+            total_expense = sum(list(map(lambda x: x["expense"], self.data))) if category is None else sum(list(map(lambda x: x["expense"], list(filter(lambda x: x["category"] == category, self.data)))))
             print(f"Total expense: ${total_expense}")
 
         elif month not in range(1, 13):
             print(f"Month {month} is not valid [Between 1 and 12]")
 
         else:
+
             total_expense = sum(
                 list(
                     map(
                         lambda x:
                             x["expense"]
                             if datetime.fromisoformat(x["date"]).month == month else 0
-                        , self.data)))
+                        , self.data))) if category is None else sum(
+                list(
+                    map(
+                        lambda x:
+                            x["expense"]
+                            if datetime.fromisoformat(x["date"]).month == month else 0
+                        , list(filter(lambda x: x["category"] == category, self.data)))))
             print(f"Total expense: ${total_expense}")
 
 
-    def display_expenses(self):
-        print(tabulate(self.data, headers="keys"))
+    def display_expenses(self, month: int | None = None, category: str | None = None):
+        if month not in range(1, 13):
+            print("Invalid Month")
+
+        print(tabulate(
+            list(filter(
+                lambda x: datetime.fromisoformat(x["date"]).month == month
+                , self.data)), headers="keys"))
 
 if __name__ == '__main__':
     tracker = ExpenseTracker()
     tracker.add_expense(description="Test #1", expense=100)
-    tracker.add_expense(description="Test #2", expense=200)
-    tracker.add_expense(description="Test #3", expense=300)
+    tracker.add_expense(description="Test #2", expense=200, category="food")
+    tracker.add_expense(description="Test #3", expense=300, category="food")
     tracker.add_expense(description="Test #4", expense=400)
-    tracker.add_expense(description="Test #5", expense=500)
+    tracker.add_expense(description="Test #5", expense=500, category="food")
     tracker.add_expense(description="Test #6", expense=600)
     tracker.add_expense(description="Test #7", expense=700)
-    tracker.add_expense(description="Test #8", expense=800)
-    tracker.add_expense(description="Test #9", expense=900)
+    tracker.add_expense(description="Test #8", expense=800, category="payment")
+    tracker.add_expense(description="Test #9", expense=900, category="payment")
 
     tracker.delete_expense(expense_id=1)
     tracker.delete_expense(expense_id=2)
@@ -143,6 +157,7 @@ if __name__ == '__main__':
     tracker.update_expense(expense_id=4, description="Test #4 (updated)", expense=400)
     tracker.update_expense(expense_id=5, description="Test #5 (updated)")
     tracker.update_expense(expense_id=6, expense=900000)
+    tracker.update_expense(expense_id=7, category="payemnt")
 
     tracker.display_expenses()
     tracker.summary_expense()
@@ -150,3 +165,4 @@ if __name__ == '__main__':
     tracker.summary_expense(month=3)
     tracker.summary_expense(month=14)
     tracker.summary_expense(month=-15)
+    tracker.summary_expense(month=4, category="payment")
